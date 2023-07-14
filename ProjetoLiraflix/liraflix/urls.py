d0f86aa7ddd03567d07a74f3406c8a12a34bcf52
Raphael Lira <97459786+Raphael-Lira@users.noreply.github.com>
2023-07-14 17:06:36 -0300

    v0.3

--- ProjetoLiraflix/liraflix/urls.py
+++ ProjetoLiraflix/liraflix/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 # Controle de URLS
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tattoo.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
